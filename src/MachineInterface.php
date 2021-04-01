65ef9634cb9f18347a70c20e7f116fbaed6ae3c9
Jon Cram <webignition@gmail.com>
2021-04-01 15:15:03 +0100

    Import (#3)

---
+++ src/MachineInterface.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace webignition\BasilWorkerManagerInterfaces;
+
+interface MachineInterface extends \JsonSerializable
+{
+    public const STATE_CREATE_RECEIVED = 'create/received';
+    public const STATE_CREATE_REQUESTED = 'create/requested';
+    public const STATE_CREATE_FAILED = 'create/failed';
+    public const STATE_UP_STARTED = 'up/started';
+    public const STATE_UP_ACTIVE = 'up/active';
+    public const STATE_DELETE_RECEIVED = 'delete/received';
+    public const STATE_DELETE_REQUESTED = 'delete/requested';
+    public const STATE_DELETE_FAILED = 'delete/failed';
+    public const STATE_DELETE_DELETED = 'delete/deleted';
+
+    public const PRE_ACTIVE_STATES = [
+        self::STATE_CREATE_RECEIVED,
+        self::STATE_CREATE_REQUESTED,
+        self::STATE_UP_STARTED,
+    ];
+
+    public const END_STATES = [
+        self::STATE_CREATE_FAILED,
+        self::STATE_DELETE_FAILED,
+        self::STATE_DELETE_DELETED,
+    ];
+
+    public function getId(): string;
+    public function getRemoteId(): ?int;
+    public function setRemoteId(int $remoteId): MachineInterface;
+
+    /**
+     * @return ProviderInterface::NAME_*
+     */
+    public function getProvider(): string;
+    public function getName(): string;
+
+    /**
+     * @return MachineInterface::STATE_*
+     */
+    public function getState(): string;
+
+    /**
+     * @param MachineInterface::STATE_* $state
+     */
+    public function setState(string $state): MachineInterface;
+
+    /**
+     * @return string[]
+     */
+    public function getIpAddresses(): array;
+
+    /**
+     * @param string[] $ipAddresses
+     */
+    public function setIpAddresses(array $ipAddresses): MachineInterface;
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function jsonSerialize(): array;
+}
