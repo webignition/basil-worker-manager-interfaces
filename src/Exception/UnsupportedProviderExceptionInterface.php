85eba5042a05c35d94ab01b782aa18cf35370f68
Jon Cram <webignition@gmail.com>
2021-04-06 16:04:37 +0100

    UnsupportedProviderExceptionInterface::getProvider() can return any string

--- src/Exception/UnsupportedProviderExceptionInterface.php
+++ src/Exception/UnsupportedProviderExceptionInterface.php
@@ -2,12 +2,7 @@
 
 namespace webignition\BasilWorkerManagerInterfaces\Exception;
 
-use webignition\BasilWorkerManagerInterfaces\ProviderInterface;
-
 interface UnsupportedProviderExceptionInterface extends \Throwable
 {
-    /**
-     * @return ProviderInterface::NAME_*
-     */
     public function getProvider(): string;
 }
