4945bafe28253878e78ca404c0d9cb7f58c59961
Jon Cram <webignition@gmail.com>
2021-04-06 14:54:53 +0100

    UnprocessableRequestExceptionInterface extends ExceptionInterface (#15)

--- src/Exception/MachineProvider/UnprocessableRequestExceptionInterface.php
+++ src/Exception/MachineProvider/UnprocessableRequestExceptionInterface.php
@@ -2,7 +2,7 @@
 
 namespace webignition\BasilWorkerManagerInterfaces\Exception\MachineProvider;
 
-interface UnprocessableRequestExceptionInterface
+interface UnprocessableRequestExceptionInterface extends ExceptionInterface
 {
     public const CODE_REMOTE_PROVIDER_RESOURCE_LIMIT_REACHED = 100;
     public const REASON_REMOTE_PROVIDER_RESOURCE_LIMIT_REACHED = 'remote provider resource limit reached';
