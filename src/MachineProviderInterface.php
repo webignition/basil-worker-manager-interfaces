becbf4ff418e0a6bb376d57144c26dab24bb06ab
Jon Cram <webignition@gmail.com>
2021-04-19 16:31:07 +0100

    Remove MachineProvider::getRemoteId(), ::setRemoteId() (#49)

--- src/MachineProviderInterface.php
+++ src/MachineProviderInterface.php
@@ -5,8 +5,6 @@ namespace webignition\BasilWorkerManagerInterfaces;
 interface MachineProviderInterface
 {
     public function getId(): string;
-    public function getRemoteId(): ?int;
-    public function setRemoteId(int $remoteId): void;
 
     /**
      * @return ProviderInterface::NAME_*
